15ce2fac8c2e490b34c1c78fe691a80b8c69b740
Hai Duong <haiduong@MacBook-Air-cua-Hai.local>
2023-03-03 20:29:28 +0700

    create model

---
+++ app/Models/productImport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class productImport extends Model
+{
+    use HasFactory;
+
+    protected $table = 'productImports';
+}
